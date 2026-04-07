fedf59fcf161f51faf80a7590ab686b22b600135
andrey_velsk <andrey_velsk@mail.ru>
2026-04-07 15:58:46 +0300

    temporary available ws server everywhere

--- plugin.cpp
+++ plugin.cpp
@@ -13,7 +13,7 @@ using     tcp  = asio::ip::tcp;
 static asio::io_context          g_ioc;
 static std::unique_ptr<WsServer> g_server;
 static std::thread               g_ioThread;
-
+s
 SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
     SKSE::Init(skse);
@@ -21,7 +21,7 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
 
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* msg) {
         if (msg->type == SKSE::MessagingInterface::kPostLoadGame && !g_server) {
-            tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1"), WEBSOCKET_PORT);
+            tcp::endpoint endpoint(asio::ip::make_address("0.0.0.0"), WEBSOCKET_PORT);
             g_server    = std::make_unique<WsServer>(g_ioc, endpoint);
             g_ioThread  = std::thread([] { g_ioc.run(); });
             g_ioThread.detach();
